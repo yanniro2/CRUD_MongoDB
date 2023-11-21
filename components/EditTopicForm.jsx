672009033eba9780bf60341fd3cebe4c784560d4
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-11-21 13:58:13 +0530

    handle change workked

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,19 +1,27 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function EditTopicForm({ id, title, description }) {
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
 
-  const router = useRouter();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "title") {
+      setNewTitle(value);
+    } else if (name === "description") {
+      setNewDescription(value);
+    }
+  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    handleSubmit();
+  }, [newTitle, newDescription]);
 
+  const handleSubmit = async () => {
     try {
-      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      const res = await fetch(`http://localhost:3001/api/topics/${id}`, {
         method: "PUT",
         headers: {
           "Content-type": "application/json",
@@ -24,35 +32,36 @@ export default function EditTopicForm({ id, title, description }) {
       if (!res.ok) {
         throw new Error("Failed to update topic");
       }
-
-      router.refresh();
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+    // <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+    <div>
       <input
-        onChange={(e) => setNewTitle(e.target.value)}
+        onChange={handleChange}
         value={newTitle}
+        name="title"
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"
       />
 
       <input
-        onChange={(e) => setNewDescription(e.target.value)}
+        onChange={handleChange}
         value={newDescription}
+        name="description"
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Description"
       />
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-        Update Topic
-      </button>
-    </form>
+      {/* <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+          Update Topic
+        </button> */}
+    </div>
+    // {/* </form> */}
   );
 }
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,10 +1,12 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch("http://localhost:3001/api/topics", {
       cache: "no-store",
     });
 
@@ -18,16 +20,31 @@ const getTopics = async () => {
   }
 };
 
-export default async function TopicsList() {
-  const { topics } = await getTopics();
+const TopicsList = () => {
+  const [topics, setTopics] = useState([]);
+
+  const fetchTopics = async () => {
+    const { topics } = await getTopics();
+    setTopics(topics);
+  };
+
+  useEffect(() => {
+    fetchTopics(); // Fetch topics initially
+
+    const intervalId = setInterval(() => {
+      fetchTopics(); // Fetch topics every 1 second
+    }, 100);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
     <>
       {topics.map((t) => (
         <div
           key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
           <div>
             <h2 className="font-bold text-2xl">{t.title}</h2>
             <div>{t.description}</div>
@@ -43,4 +60,6 @@ export default async function TopicsList() {
       ))}
     </>
   );
-}
+};
+
+export default TopicsList;
